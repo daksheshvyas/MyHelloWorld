1d2b894142094c7d1d48757dcde4f46df9f62641
daksheshvyas <daksheshvyas@yahoo.com>
2014-09-26 10:47:11 -0700

    Update scan_script.sh

--- scan_script.sh
+++ scan_script.sh
@@ -52,10 +52,6 @@ if [ ! -d $TOOL_BASE ]; then
   # Download Coverity Scan Analysis Tool
   if [ ! -e $TOOL_ARCHIVE ]; then
     echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
-    echo "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
-    echo wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data \"project=$COVERITY_SCAN_PROJECT_NAME\&token=$COVERITY_SCAN_TOKEN\"
-    traceroute scan.coverity.com
-    
     wget -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
   fi
 
