2ac319cee8a6044f847eb203411430f31002963e
daksheshvyas <daksheshvyas@yahoo.com>
2015-07-08 09:25:35 -0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -7,7 +7,7 @@ int main(){
         for(i=0;i<x;i++)
                 printf("Hello World");
 
-        printf("Hello World is printed %d times",x);
+        printf("Hello World is printed %d times and program end",x);
 
         return 0;
 
