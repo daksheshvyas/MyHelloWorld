3d12e1dbf5652c572e93ff9252855133b74c7581
daksheshvyas <daksheshvyas@yahoo.com>
2015-07-08 10:59:31 -0700

    Update scan_script.sh

--- scan_script.sh
+++ scan_script.sh
@@ -44,7 +44,7 @@ else
   else
     WHEN=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['next_upload_permitted_at']"`
     echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
-    exit 
+    return 
   fi
 fi
 
